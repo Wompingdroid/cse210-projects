7694af6a1b336d60fff6ffc3b4b0791d0731802a
Andrew Nelson <wompingdroid@gmail.com>
2024-01-15 15:31:02 -0700

    random number upload

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int majNum = randomGenerator.Next(1, 101);
+        string guessNum = "";
+
+        do{
+            System.Console.Write("guess a number: ");
+            guessNum = Console.ReadLine();
+
+            if(int.Parse(guessNum) > majNum)
+            {
+                System.Console.WriteLine("Lower");
+            }
+            else if (int.Parse(guessNum) < majNum)
+            {
+                System.Console.WriteLine("Higher");
+            }
+            else
+            {
+                System.Console.WriteLine("Good Job!!");
+            }
+        } while (majNum != int.Parse(guessNum));
+
     }
 }
