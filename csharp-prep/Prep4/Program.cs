d1d55f10104a529341dee95835d60a6a74c5ecbd
Andrew Nelson <wompingdroid@gmail.com>
2024-01-17 09:57:37 -0700

    prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        string num = "";
+        double i = 0;
+        int totalAdd = 0;
+        double average = 0;
+        int largest = 0;
+        System.Console.WriteLine("Please input numbers. When you input 0 the program will end.");
+        do
+        {
+            num = Console.ReadLine();
+            numbers.Add(int.Parse(num));
+            i++;
+        }while (num != "0");
+        i = i-1;
+        foreach (var n in numbers)
+        {
+            totalAdd += n;
+
+            if (n >= largest)
+            {
+                largest = n;
+            }
+        }
+        average = totalAdd / i;
+
+        System.Console.WriteLine($"The total addition of your inputs is {totalAdd}");
+        System.Console.WriteLine($"The average of all your inputs = {average}");
+        System.Console.WriteLine($"The largest number added was {largest}");
+
     }
 }
