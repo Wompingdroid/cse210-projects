f7ee3dd9f886e74c54c347794704089e0f119585
Andrew Nelson <wompingdroid@gmail.com>
2024-01-20 18:47:20 -0700

    prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+    //define all functions
+    static void Welcome()
+    {
+        System.Console.WriteLine("Welcome to the program!");
+    }
+
+
+    string GetName()
+    {
+       System.Console.Write("Please enter your name: ");
+       string name = Console.ReadLine(); 
+       return name;
+    }
+
+
+    int GetNum()
+    {
+       System.Console.Write("Please enter your favorite number: ");
+       string num = Console.ReadLine(); 
+       return int.Parse(num); 
+    }
+
+
+    int SquareNum(int number)
+    {
+        int finalNumber = number * number;
+        return finalNumber;
+    }
+
+    //call all functions in the order of the assignment
+    Welcome();
+    string name = GetName();
+    int num = GetNum();
+    System.Console.WriteLine($"Your favorite number squared is {SquareNum(num)}");
+    
+
     }
 }
