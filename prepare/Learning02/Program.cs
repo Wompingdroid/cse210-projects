c227f37b12fa79b516dff82d3f214a22f2a2b36f
Andrew Nelson <wompingdroid@gmail.com>
2024-01-22 17:54:36 -0700

    did assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
-}
+Job job1 = new Job();
+job1._company = "Microsoft";
+job1._jobTitle = "Software engineer";
+job1._startYear = 2020;
+job1._endYear = 2021;
+
+Job job2 = new Job();
+job2._company = "Apple";
+job2._jobTitle = "Sales associate";
+job2._startYear = 2022;
+job2._endYear = 2023;
+
+Resume myResume = new Resume();
+myResume._name = "Andrew Nelson";
+
+myResume._jobs.Add(job1);
+myResume._jobs.Add(job2);
+
+myResume.Display();
